b0d61c1a8b88b6221364c85218255cbbe4eaee3e
Deya Chatterjee <28012711+deyachatterjee@users.noreply.github.com>
2018-01-12 00:55:40 +0530

    Add files via upload

---
+++ ex6/ex6spam.py
@@ -0,0 +1,205 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io #Used to load the OCTAVE *.mat files
+from sklearn import svm #SVM software
+import re 
+import nltk, nltk.stem.porter
+
+print "emailSample1.txt:"
+!cat data/emailSample1.txt
+
+"""
+Anyone knows how much it costs to host a web portal ?
+>
+Well, it depends on how many visitors you're expecting.
+This can be anywhere from less than 10 bucks a month to a couple of $100. 
+You should checkout http://www.rackspace.com/ or perhaps Amazon EC2 
+if youre running something big..
+
+To unsubscribe yourself from this mailing list, send an email to:
+groupname-unsubscribe@egroups.com
+"""
+
+def preProcess( email ):
+    """
+    Function to do some pre processing (simplification of e-mails).
+    Comments throughout implementation describe what it does.
+    Input = raw e-mail
+    Output = processed (simplified) email
+    """
+    # Make the entire e-mail lower case
+    email = email.lower()
+    
+    # Strip html tags (strings that look like <blah> where 'blah' does not
+    # contain '<' or '>')... replace with a space
+    email = re.sub('<[^<>]+>', ' ', email);
+    
+    #Any numbers get replaced with the string 'number'
+    email = re.sub('[0-9]+', 'number', email)
+    
+    #Anything starting with http or https:// replaced with 'httpaddr'
+    email = re.sub('(http|https)://[^\s]*', 'httpaddr', email)
+    
+    #Strings with "@" in the middle are considered emails --> 'emailaddr'
+    email = re.sub('[^\s]+@[^\s]+', 'emailaddr', email);
+    
+    #The '$' sign gets replaced with 'dollar'
+    email = re.sub('[$]+', 'dollar', email);
+    
+    return email
+	
+	
+def email2TokenList( raw_email ):
+    """
+    Function that takes in preprocessed (simplified) email, tokenizes it,
+    stems each word, and returns an (ordered) list of tokens in the e-mail
+    """
+    
+    # I'll use the NLTK stemmer because it more accurately duplicates the
+    # performance of the OCTAVE implementation in the assignment
+    stemmer = nltk.stem.porter.PorterStemmer()
+    
+    email = preProcess( raw_email )
+
+    #Split the e-mail into individual words (tokens) (split by the delimiter ' ')
+    #but also split by delimiters '@', '$', '/', etc etc
+    #Splitting by many delimiters is easiest with re.split()
+    tokens = re.split('[ \@\$\/\#\.\-\:\&\*\+\=\[\]\?\!\(\)\{\}\,\'\"\>\_\<\;\%]', email)
+    
+    #Loop over each word (token) and use a stemmer to shorten it,
+    #then check if the word is in the vocab_list... if it is,
+    #store what index in the vocab_list the word is
+    tokenlist = []
+    for token in tokens:
+        
+        #Remove any non alphanumeric characters
+        token = re.sub('[^a-zA-Z0-9]', '', token);
+
+        #Use the Porter stemmer to stem the word
+        stemmed = stemmer.stem( token )
+        
+        #Throw out empty tokens
+        if not len(token): continue
+            
+        #Store a list of all unique stemmed words
+        tokenlist.append(stemmed)
+            
+    return tokenlist
+
+#vocabulaty list
+
+def getVocabDict(reverse=False):
+    """
+    Function to read in the supplied vocab list text file into a dictionary.
+    I'll use this for now, but since I'm using a slightly different stemmer,
+    I'd like to generate this list myself from some sort of data set...
+    Dictionary key is the stemmed word, value is the index in the text file
+    If "reverse", the keys and values are switched.
+    """
+    vocab_dict = {}
+    with open("data/vocab.txt") as f:
+        for line in f:
+            (val, key) = line.split()
+            if not reverse:
+                vocab_dict[key] = int(val)
+            else:
+                vocab_dict[int(val)] = key
+                
+    return vocab_dict
+
+	
+def email2VocabIndices( raw_email, vocab_dict ):
+    """
+    Function that takes in a raw email and returns a list of indices corresponding
+    to the location in vocab_dict for each stemmed word in the email.
+    """
+    tokenlist = email2TokenList( raw_email )
+    index_list = [ vocab_dict[token] for token in tokenlist if token in vocab_dict ]
+    return index_list
+	
+	
+#feature extraction
+
+def email2FeatureVector( raw_email, vocab_dict ):
+    """
+    Function that takes as input a raw email, and returns a vector of shape
+    (n,1) where n is the size of the vocab_dict.
+    The first element in this vector is 1 if the vocab word with index == 1
+    is in the raw_email, 0 otherwise.
+    """
+    n = len(vocab_dict)
+    result = np.zeros((n,1))
+    vocab_indices = email2VocabIndices( email_contents, vocab_dict )
+    for idx in vocab_indices:
+        result[idx] = 1
+    return result
+	
+# the feature vector has length 1899 and 45 non-zero entries."
+
+vocab_dict = getVocabDict()
+email_contents = open( 'data/emailSample1.txt', 'r' ).read()
+test_fv = email2FeatureVector( email_contents, vocab_dict )
+
+print "Length of feature vector is %d" % len(test_fv)
+print "Number of non-zero entries is: %d" % sum(test_fv==1)
+
+
+
+#svm for spam classification
+datafile = 'data/spamTrain.mat'
+mat = scipy.io.loadmat( datafile )
+X, y = mat['X'], mat['y']
+#NOT inserting a column of 1's in case SVM software does it for me automatically...
+#X =     np.insert(X    ,0,1,axis=1)
+
+# Test set
+datafile = 'data/spamTest.mat'
+mat = scipy.io.loadmat( datafile )
+Xtest, ytest = mat['Xtest'], mat['ytest']
+pos = np.array([X[i] for i in xrange(X.shape[0]) if y[i] == 1])
+neg = np.array([X[i] for i in xrange(X.shape[0]) if y[i] == 0])
+print 'Total number of training emails = ',X.shape[0]
+print 'Number of training spam emails = ',pos.shape[0]
+print 'Number of training nonspam emails = ',neg.shape[0]
+
+
+# First we make an instance of an SVM with C=0.1 and 'linear' kernel
+linear_svm = svm.SVC(C=0.1, kernel='linear')
+
+# Now we fit the SVM to our X matrix, given the labels y
+linear_svm.fit( X, y.flatten() )
+
+
+#  training accuracy of about 99.8% and a test accuracy of about 98.5%"
+
+train_predictions = linear_svm.predict(X).reshape((y.shape[0],1))
+train_acc = 100. * float(sum(train_predictions == y))/y.shape[0]
+print 'Training accuracy = %0.2f%%' % train_acc
+
+test_predictions = linear_svm.predict(Xtest).reshape((ytest.shape[0],1))
+test_acc = 100. * float(sum(test_predictions == ytest))/ytest.shape[0]
+print 'Test set accuracy = %0.2f%%' % test_acc
+
+# Determine the words most likely to indicate an e-mail is a spam
+# From the trained SVM we can get a list of the weight coefficients for each
+# word (technically, each word index)
+
+vocab_dict_flipped = getVocabDict(reverse=True)
+
+#Sort indicies from most important to least-important (high to low weight)
+sorted_indices = np.argsort( linear_svm.coef_, axis=None )[::-1]
+print "The 15 most important words to classify a spam e-mail are:"
+print [ vocab_dict_flipped[x] for x in sorted_indices[:15] ]
+print
+print "The 15 least important words to classify a spam e-mail are:"
+print [ vocab_dict_flipped[x] for x in sorted_indices[-15:] ]
+print
+
+# Most common word (mostly to debug):
+most_common_word = vocab_dict_flipped[sorted_indices[0]]
+print '# of spam containing \"%s\" = %d/%d = %0.2f%%'% \
+    (most_common_word, sum(pos[:,1190]),pos.shape[0],  \
+     100.*float(sum(pos[:,1190]))/pos.shape[0])
+print '# of NON spam containing \"%s\" = %d/%d = %0.2f%%'% \
+    (most_common_word, sum(neg[:,1190]),neg.shape[0],      \
+     100.*float(sum(neg[:,1190]))/neg.shape[0])
---
+++ ex6/ex6svm.py
@@ -0,0 +1,184 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from functions import (
+    gaussian_kernel, dataset3_params
+)
+
+#necessary functions
+# Your task is to use the cross validation set Xval, yval to 
+# determine the best C and σ parameter to use.
+
+def dataset3_params(X, y, Xval, yval):
+    C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    scores = np.zeros((len(C_vec), len(sigma_vec)))
+
+    for i in range(len(C_vec)):
+        for j in range(len(sigma_vec)):
+            svm = SVC(kernel='rbf', C=C_vec[i], gamma=sigma_vec[j])
+            svm.fit(X, y.ravel())
+            scores[i, j] = accuracy_score(yval, svm.predict(Xval))
+
+    max_c_index, max_s_index = np.unravel_index(scores.argmax(), scores.shape)
+    return (C_vec[max_c_index], sigma_vec[max_s_index])
+	
+	
+	
+
+# Loading and Visualizing Data 
+print('Loading and Visualizing Data ...\n')
+
+data = sio.loadmat('ex6data1.mat')
+X = data['X']  # 51 x 2 matrix
+y = data['y']  # 51 x 1 matrix
+
+pos = (y == 1).ravel()         #flattens i.e. makes 1d array
+neg = (y == 0).ravel()         #alternative code neg = np.array([X[i] for i in xrange(X.shape[0]) if y[i] == 0])
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.xlim(0, 4.5)
+plt.ylim(1.5, 5)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+#Training Linear SVM 
+print('Training Linear SVM ...\n')
+
+C = 1
+svm = SVC(kernel='linear', C=C)
+svm.fit(X, y.ravel())
+weights = svm.coef_[0]
+intercept = svm.intercept_[0]
+#draw svm boundary
+xp = np.linspace(X.min(), X.max(), 100)
+yp = - (weights[0] * xp + intercept) / weights[1]
+
+pos = (y == 1).ravel()
+neg = (y == 0).ravel()
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.plot(xp, yp)
+plt.xlim(0, 4.5)
+plt.ylim(1.5, 5)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+#Gaussian Kernel
+print('Evaluating the Gaussian Kernel ...\n')
+
+#linalg.norm returns one of seven different matrix norms
+def gaussian_kernel(x1, x2, sigma):
+    return np.exp(- (np.linalg.norm(x1 - x2) ** 2).sum() / (2 * (sigma ** 2)))
+
+
+x1 = np.array([1, 2, 1])
+x2 = np.array([0, 4, -1])
+sigma = 2
+sim = gaussian_kernel(x1, x2, sigma)
+
+
+
+print(
+    'Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = {0} :\n'
+    .format(sigma),
+    '\t{0:.6f}\n(for sigma = 2, this value should be about 0.324652)'
+    .format(sim))
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+#Visualizing Dataset 2 
+data = sio.loadmat('ex6data2.mat')
+X = data['X']  # 863 x 2 matrix
+y = data['y']  # 863 x 1 matrix
+
+pos = (y == 1).ravel()
+neg = (y == 0).ravel()
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.xlim(0, 1)
+plt.ylim(0.4, 1)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+#RBF Kernel (Dataset 2)
+print('Training SVM with RBF Kernel ...\n')
+
+C = 30
+sigma = 30
+
+svm = SVC(kernel='rbf', C=C, gamma=sigma)
+svm.fit(X, y.ravel())
+
+x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
+x2 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+x1, x2 = np.meshgrid(x1, x2)
+yp = svm.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape)
+
+pos = (y == 1).ravel()
+neg = (y == 0).ravel()
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.xlim(0, 1)
+plt.ylim(0.4, 1)
+plt.contour(x1, x2, yp)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+# Visualizing Dataset 3 
+data = sio.loadmat('ex6data3.mat')
+X = data['X']  # 211 x 2 matrix
+y = data['y']  # 211 x 1 matrix
+Xval = data['Xval']  # 200 x 2 matrix
+yval = data['yval']  # 200 x 1 matrix
+
+pos = (y == 1).ravel()
+neg = (y == 0).ravel()
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.xlim(-0.6, 0.3)
+plt.ylim(-0.8, 0.6)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
+
+# RBF Kernel (Dataset 3)
+C, sigma = dataset3_params(X, y, Xval, yval)
+
+svm = SVC(kernel='rbf', C=C, gamma=sigma)
+svm.fit(X, y.ravel())
+
+x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
+x2 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+x1, x2 = np.meshgrid(x1, x2)
+yp = svm.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape)
+
+pos = (y == 1).ravel()
+neg = (y == 0).ravel()
+plt.scatter(X[pos, 0], X[pos, 1], color='black', marker='+')
+plt.scatter(
+    X[neg, 0], X[neg, 1], color='yellow', edgecolors='black', marker='o')
+plt.xlim(-0.6, 0.3)
+plt.ylim(-0.8, 0.6)
+plt.contour(x1, x2, yp)
+plt.show()
+
+input('Program paused. Press enter to continue.\n')
+plt.close()
